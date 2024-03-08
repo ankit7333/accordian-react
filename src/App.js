2bf88c4c5e0f5d0d77f9d8db7184755dae4b4d52
ankitadulkar <ankit.adulkar1@justdial.com>
2024-03-08 15:05:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import "./index.css";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({data}){
+  const [currOpen, setCurrOpen] = useState(null);
+  
+  return(
+    <div className="accordion">
+      {
+        data.map((item, index) => (
+            <AccordionItem 
+              currentOpen={currOpen} 
+              onOpen={setCurrOpen} 
+              key={index} 
+              num={index} 
+              title={item.title}
+            >{item.text}</AccordionItem>
+          
+        ))
+      }
+      <AccordionItem 
+            currentOpen={currOpen} 
+            onOpen={setCurrOpen} 
+            key='Test 1' 
+            num={23}
+            title='Test 1'
+          >
+            <p>Allows React developers to:</p>
+            <ul>
+              <li>Break up UI into components</li>
+              <li>Make components reusable</li>
+              <li>Place state efficiently</li>
+            </ul>
+          </AccordionItem>
+    </div>
+  )
+}
+
+function AccordionItem({num, title, currentOpen, onOpen, children}) {
+  const isOpen = num === currentOpen;
+  const handleToggle = () => {
+    onOpen(isOpen ? null : num);
+  }
+  return(
+    <div className={`item ${isOpen ? 'open' : null}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content-box">{children}</div> }
+    </div>
+  )
+}
